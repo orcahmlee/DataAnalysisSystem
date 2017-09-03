f6923b6cbdf3f659c5f23569ac8c1f747d3daffb
Andrew <orcahmlee@gmail.com>
2017-09-03 12:09:23 +0800

    20170903_Added a function that can save the selected data to a ".csv" file.

---
+++ WebContent/saveFile.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+<head>
+	<script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>YOUR FACTORY</title>
+	<style type="text/css">
+        body {
+    		padding-top: 70px;
+    	}
+    </style>	
+</head>
+<body>
+File Already Be Saved, Reload in 3 seconds...!
+
+<script type = 'text/javascript'>
+function redirect() {
+	window.location.assign('http://localhost:8080/DataAnalysisSystem/FactoryMainMachine1_History');
+}
+
+document.ready(setTimeout(redirect, 3000));
+</script>
+</html>
---
+++ src/tw/andrew/SaveFile.java
@@ -0,0 +1,63 @@
+package tw.andrew;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/SaveFile")
+public class SaveFile extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; chartset = UTF-8");
+		PrintWriter out = response.getWriter();
+
+		HttpSession session = request.getSession(false);
+		LinkedList<HashMap<String, String>> dataLinkedMap = (LinkedList<HashMap<String, String>>)session.getAttribute("dataLinkedMap");
+		StringBuffer sb = new StringBuffer();
+		sb = sb.append("Date").append(", ").append("Time").append(", ").append("Temperature").append(", ").append("Pressure").append(", ").append("FlowRate").append("\n");
+		
+		for (int i = 0; i < dataLinkedMap.size(); i++) {
+			String date = dataLinkedMap.get(i).get("date");
+			String time = dataLinkedMap.get(i).get("time");
+			String temperature = dataLinkedMap.get(i).get("temperature");
+			String pressure = dataLinkedMap.get(i).get("pressure");
+			String flowrate = dataLinkedMap.get(i).get("flowrate");	
+			sb = sb.append(date).append(",").append(time).append(",").append(temperature).append(",").append(pressure).append(",").append(flowrate).append("\n");
+		}
+		
+		String data = sb.toString();
+		DateTime dt = new DateTime();
+		String saveTime = dt.getSaveTime();
+		String fileName = saveTime + ".csv";
+
+		File record = new File("/Users/Andrew/Programming/Java/DataAnalysisSystem/Record", fileName);
+		FileOutputStream fout = new FileOutputStream(record);
+		BufferedOutputStream bout = new BufferedOutputStream(fout);
+		bout.write(data.getBytes());
+		bout.flush();
+		bout.close();
+
+		// TODO Has to redirect to "somewhere page" after saving file.
+		// VIEW
+		// Send the request, response, and session(including the data) to the VIEW.
+		RequestDispatcher rd = request.getRequestDispatcher("saveFile.jsp");
+		rd.forward(request, response);
+
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
